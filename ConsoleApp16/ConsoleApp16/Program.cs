837330753fc55508f650dec1e2dcb847e386db8e
Shaik Muneer <shaik.muneer@aveva.com>
2021-12-12 13:20:54 +0530

    Adding consoleapp16

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "shaik@muneer@hello";
+            
+            string[] arr = str.Split('@');
+
+
+                    Console.WriteLine(arr[0]);
+                
+            Console.ReadKey();
+            }
+        }
+
+    }
+
+        
+    
+
+
+
